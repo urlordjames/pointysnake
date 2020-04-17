913424ea2a66ed96b29f43392f2d436919e519ce
urlordjames <urlordjames@gmail.com>
2020-04-17 17:52:29 -0400

    made precompiling work

--- parse.py
+++ parse.py
@@ -15,7 +15,7 @@ def parseline(line):
         return line
     if line[0][0] == "function":
         if line[1][0] == "string" or line[1][0] == "int":
-            return ["call", line[0][1], parseline(line[1][1])]
+            return ["call", line[0][1], [parseline(line[1][1])]]
     return -1
 
 if __name__ == "__main__":
--- precompile.py
+++ precompile.py
@@ -1,7 +1,13 @@
 from parse import parse
 
 def psncompile(filename):
-    return parse(filename)
+    parsed = parse(filename)
+    print(parsed)
+    f = open("makebin/bin/Debug/precomp.psnbin", "w")
+    for line in parsed:
+        if line[0] == "call":
+            f.write(str(line[2]) + "\n" + line[0] + ", " + line[1] + "\n")
+    f.close()
 
 if __name__ == "__main__":
-    print(psncompile("test/consoleout.psn"))
+    psncompile("test/consoleout.psn")
