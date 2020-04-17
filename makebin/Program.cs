f2537728237a6c56a485c4db29520474b2c2b2ca
urlordjames <urlordjames@gmail.com>
2020-04-17 18:38:59 -0400

    made things just work

--- makebin/Program.cs
+++ makebin/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using dnlib;
+using System.IO;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 
 namespace makebin
 {
@@ -7,7 +9,53 @@ namespace makebin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            String[] file = File.ReadAllLines("precomp.psnbin");
+            var mod = new ModuleDefUser("PointySnakeModule");
+            mod.Kind = ModuleKind.Console;
+            var asm = new AssemblyDefUser("PointySnakeAssembly");
+            asm.Modules.Add(mod);
+            var startUpType = new TypeDefUser("PointySnake", "Program", mod.CorLibTypes.Object.TypeDefOrRef);
+            mod.Types.Add(startUpType);
+            var entryPoint = new MethodDefUser("Main", MethodSig.CreateStatic(mod.CorLibTypes.Int32, new SZArraySig(mod.CorLibTypes.String)));
+            entryPoint.Attributes = MethodAttributes.Private | MethodAttributes.Static | MethodAttributes.HideBySig | MethodAttributes.ReuseSlot;
+            entryPoint.ImplAttributes = MethodImplAttributes.IL | MethodImplAttributes.Managed;
+            entryPoint.ParamDefs.Add(new ParamDefUser("args", 1));
+            var epBody = new CilBody();
+            entryPoint.Body = epBody;
+            startUpType.Methods.Add(entryPoint);
+            mod.EntryPoint = entryPoint;
+            foreach (String line in file) {
+                WriteInstruction(line, mod, entryPoint.Body);
+            }
+            entryPoint.Body.Instructions.Add(OpCodes.Endfinally.ToInstruction());
+            entryPoint.Body.OptimizeBranches();
+            entryPoint.Body.OptimizeMacros();
+            Console.WriteLine("done");
+            mod.Write("assembly.exe");
+            #if DEBUG
+            Console.WriteLine("press any key to close...");
+            Console.ReadKey();
+            #endif
+        }
+
+        public static void WriteInstruction(String line, ModuleDefUser mod, CilBody epBody)
+        {
+            String[] splitline = line.Split(new String[] {", "}, StringSplitOptions.None);
+            if (splitline[0] == "ldstr")
+            {
+                epBody.Instructions.Add(OpCodes.Ldstr.ToInstruction(splitline[1]));
+                return;
+            }
+            if (splitline[0] == "call")
+            {
+                if (splitline[1] == "print") {
+                    var consoleRef = new TypeRefUser(mod, "System", "Console", mod.CorLibTypes.AssemblyRef);
+                    var consoleWrite1 = new MemberRefUser(mod, "WriteLine", MethodSig.CreateStatic(mod.CorLibTypes.Void, mod.CorLibTypes.String), consoleRef);
+                    epBody.Instructions.Add(OpCodes.Call.ToInstruction(consoleWrite1));
+                }
+                return;
+            }
+            Console.WriteLine(line);
         }
     }
 }
--- precompile.py
+++ precompile.py
@@ -6,7 +6,7 @@ def psncompile(filename):
     f = open("makebin/bin/Debug/precomp.psnbin", "w")
     for line in parsed:
         if line[0] == "call":
-            f.write(str(line[2]) + "\n" + line[0] + ", " + line[1] + "\n")
+            f.write("ldstr, " + line[2][0] + "\n" + line[0] + ", " + line[1] + "\n")
     f.close()
 
 if __name__ == "__main__":
