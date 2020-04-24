767b4949205d6672f07499ff2b0ad733e619d00c
urlordjames <urlordjames@gmail.com>
2020-04-23 22:12:20 -0400

    added copyright notice and description

--- makebin/Properties/AssemblyInfo.cs
+++ makebin/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("makebin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/urlordjames/pointysnake#readme")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("makebin")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © urlordjames 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
