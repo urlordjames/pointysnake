f941e2662e8d2ff7ea8739b5934e23e04e39519e
urlordjames <urlordjames@gmail.com>
2020-07-11 20:54:20 -0400

    made sure the test is actually ran

--- runtests.sh
+++ runtests.sh
@@ -8,11 +8,19 @@ for f in ./precompiled/*.psnbin; do
     mono makebin.exe
     if [ $? -eq 0 ]
     then
-        echo "test passed"
+        echo "compiled successfully"
         rm precomp.psnbin
     else
-        echo "test failed"
+        echo "compilation failure"
         cat precomp.psnbin
+        exit 1
+    fi
+    mono executable.exe
+    if [ $? -eq 0 ]
+    then
+        echo "test passed"
+    else
+        echo "runtime error"
         exit 1
     fi
 done
