2fa721d780fffb147fdf330215e2edab494df840
urlordjames <urlordjames@gmail.com>
2020-07-11 20:47:42 -0400

    changed test script

--- runtests.sh
+++ runtests.sh
@@ -4,12 +4,15 @@ echo "running tests..."
 
 for f in ./precompiled/*.psnbin; do
     echo $f
-    mono makebin.exe $f
+    cp $f precomp.psnbin
+    mono makebin.exe
     if [ $? -eq 0 ]
     then
         echo "test passed"
+        rm precomp.psnbin
     else
         echo "test failed"
+        cat precomp.psnbin
         exit 1
     fi
 done
