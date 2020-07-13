1eaba29b25d34c5af94b48d6ac0eff7d040f7a65
urlordjames <urlordjames@gmail.com>
2020-07-12 22:54:28 -0400

    fixed parsing (but precompiling is still broken...)

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 from lex import lex
 
 validargs = ["str", "int", "var", "staticvar"]
+rawtypes = [str, int]
 
 def parse(filename):
     lexed = lex(filename)
@@ -13,25 +14,38 @@ def parse(filename):
     return ast
 
 def parseline(line):
-    if type(line) == str or type(line) == int:
+    if type(line) in rawtypes:
         return line
-    if line[0][0] in validargs:
-        if len(line) > 1:
+    if line[0] == "args":
+        if len(line[1]) > 1:
             buffer = []
-            for arg in line:
+            altbuffer = []
+            line[1].append(["argseperate"])
+            for arg in line[1]:
                 if arg[0] == "argseperate":
-                    continue
-                buffer.append([arg[0], arg[1]])
-            return buffer
-        return parseline(line[0][1])
+                    if len(altbuffer) > 1:
+                        buffer.append(altbuffer)
+                    else:
+                        buffer.append(altbuffer[0])
+                    altbuffer = []
+                else:
+                    altbuffer.append(arg)
+            altbuffer = []
+            for arg in buffer:
+                if arg[0] in validargs:
+                    altbuffer.append(arg)
+                else:
+                    altbuffer.append(parseline(arg))
+            return altbuffer
+        return []
     if line[0][0] == "functiondefine":
         return ["newfunc", line[1][1]]
     if line[0][0] == "functionterminate":
         return ["endfunc"]
     if line[0][0] == "setvar":
         if line[1][0] == "function":
-            return [line[0][0], "int", line[0][1], parseline(line[1:])]
-        return [line[0][0], line[1][0], line[0][1], parseline(line[1][1])]
+            return [line[0][0], "int", line[0][1], parseline(["args", line[2:-1]])]
+        return [line[0][0], line[1][0], line[0][1], parseline(["args", line[1][1]])]
     if line[0][0] == "setstaticvar":
         if line[1][0] == "function":
             print("error: staticvar cannot be set as returned value from function")
@@ -41,7 +55,7 @@ def parseline(line):
         if line[1][0] == "functionend":
             return ["call", line[0][1], []]
         else:
-            return ["call", line[0][1], [parseline(line[1:-1])]]
+            return ["call", line[0][1], [parseline(["args", line[1:-1]])]]
     if line[0][0] == "conddefine":
         return ["cond", line[1], line[3]]
     if line[0][0] == "assert":
