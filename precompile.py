dc04807e5e1fc06134cb58420f71a6b7b934e4e4
urlordjames <urlordjames@gmail.com>
2020-07-18 23:23:16 -0400

    replaced string concats with f strings in precompiler

--- precompile.py
+++ precompile.py
@@ -16,25 +16,25 @@ def psncompile(filename):
 
 def compileline(line, f):
     if line[0] in validtypes:
-        f.write("ld" + line[0] + ", " + str(line[1]) + "\n")
+        f.write(f"ld{line[0]}, {str(line[1])}\n")
     elif line[0] == "call":
         resolvecall(line, f)
     elif line[0] == "setvar":
         vartypes[line[2]] = line[1]
         compileline(line[3], f)
-        f.write("setvar, " + line[2] + ", " + line[1] + "\n")
+        f.write(f"setvar, {line[2]}, {line[1]}\n")
     elif line[0] == "setstaticvar":
         staticvars[line[2]] = [line[1], line[3]]
     elif line[0] == "newfunc":
-        f.write("newfunc, " + line[1] + "\n")
+        f.write(f"newfunc, {line[1]}\n")
     elif line[0] == "endfunc":
         f.write("endfunc\n")
     elif line[0] == "cond":
         resolvevar(line[1], f)
-        f.write(line[0] + ", " + line[2][1] + "\n")
+        f.write(f"{line[0]}, {line[2][1]}\n")
     elif line[0] == "assert":
         resolvevar(line[1], f)
-        f.write(line[0] + "\n")
+        f.write(f"{line[0]}\n")
 
 def resolvecall(line, f):
     fname = line[1]
@@ -52,16 +52,16 @@ def resolvecall(line, f):
     if fname == "return":
         if line[2] == []:
             f.write("ldint, 0\n")
-        f.write(line[0] + ", " + fname + "\n")
+        f.write(f"{line[0]}, {fname}\n")
         return
-    f.write(line[0] + ", " + fname + "\n")
+    f.write(f"{line[0]}, {fname}\n")
 
 def resolvevar(variable, f):
     if variable[0] == "var":
-        f.write("ldvar, " + variable[1] + "\n")
+        f.write(f"ldvar, {variable[1]}\n")
     else:
         staticinfo = staticvars[variable[1]]
-        f.write("ld" + staticinfo[0] + ", " + str(staticinfo[1]) + "\n")
+        f.write(f"ld {staticinfo[0]}, {str(staticinfo[1])}\n")
 
 if __name__ == "__main__":
     psncompile("tests/printstr.psn")
